ef58be5f4da8bc0478009da765af18869d9b3115
Lex Myers <lex.myers@gmail.com>
2017-10-25 16:57:00 -0700

    set up models and registration form

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -9,19 +9,43 @@ namespace logreg.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Method()
+        [HttpGet]
+        [Route("")]
+        public IActionResult Reg()
         {
             User NewUser = new User
             {
                 FirstName = "First",
                 LastName = "Last",
                 Email = "email@example.com",
-                Password = "password"
+                Password = "password",
+                Confirm = "password",
             };
 
             TryValidateModel(NewUser);
             ViewBag.errors = ModelState.Values;
             return View();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("YouAreIn", user);
+            }
+            else
+            {
+                ViewBag.errors = ModelState.Values;
+                return View("~/Views/User/Reg.cshtml", user);
+            }
+        }
+        [HttpGet]
+        [Route("in")]
+        public IActionResult YouAreIn()
+        {
+            return View("~/Views/User/YouAreIn.cshtml");
         }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -5,17 +5,26 @@ namespace logreg.Models
     public abstract class BaseEntity {}
     public class User : BaseEntity
     {
+        [Display(Name = "First Name")]
         [Required]
         [MinLength(2)]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         [Required]
         [MinLength(2)]
         public string LastName { get; set; }
+        [Display(Name = "Email")]
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+        [Display(Name = "Password")]
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Display(Name = "Confirm Password")]
+        [Required]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string Confirm { get; set; }
     }
 }
